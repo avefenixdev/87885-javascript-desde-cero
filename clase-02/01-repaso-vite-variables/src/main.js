c124362c291415aacfa89d515a0b9a3d792e410c
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-27 09:52:29 -0300

    Variables y como saber el tipo

--- clase-02/01-repaso-vite-variables/src/main.js
+++ clase-02/01-repaso-vite-variables/src/main.js
@@ -18,5 +18,27 @@ console.log('// ! Extensiones de Visual Studio Code')
 
 console.warn('// ! Variable') 
 
+console.error('// ! IMPORTANTE: NO USAR VAR PARA CONSTRUIR VARIABLES')
 
+console.warn('// ! Constructores de variables (let y const)')
 
+// ASIGNACIÓN
+let nombre = 'Maxi' // Cadena de caracteres (strings)
+console.log(nombre)
+let numero = 22 // Un número entero (number)
+console.log(numero)
+let precio = 99.99 // Un número decimal (number)
+console.log(precio)
+let cumple = true // Booleano (admite 2 valores -> true o false) | verdadero o falso
+console.log(cumple)
+
+let noDefinida // No le asigno nada
+console.log(noDefinida) // undefined
+
+console.warn('// ! Averiguar el tipo de dato de una variable')
+
+console.log(typeof nombre) // string
+console.log(typeof numero) // number
+console.log(typeof precio) // number
+console.log(typeof cumple) // boolean
+console.log(typeof noDefinida) // undefined
