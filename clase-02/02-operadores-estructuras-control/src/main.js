f199e270c0e0e871b619dc6c46f26a30daf0b584
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-27 11:51:04 -0300

    Empezando con operadores

---
+++ clase-02/02-operadores-estructuras-control/src/main.js
@@ -0,0 +1,24 @@
+import './style.css'
+
+console.warn('// ! Operadores aritméticos')
+
+let a = 4
+let b = 2
+
+let suma = a + b
+console.log(suma); // 6
+let resta = a - b
+console.log(resta); // 2
+let  multiplicacion = a * b
+console.log(multiplicacion); // 8
+let division = a / b
+console.log(division); // 2
+let modulo = a % b
+console.log(modulo); // 0
+let potencia = a**b // 4*4 = 16
+console.log(potencia);
+
+console.warn('// ! Operadores de comparación')
+
+const edad = 18
+
