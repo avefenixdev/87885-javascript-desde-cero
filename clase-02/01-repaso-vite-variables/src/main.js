8960c961ac2a03fb0cadb3f323ca583ccffa0d05
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-27 09:42:57 -0300

    Empezando con variables en JS

---
+++ clase-02/01-repaso-vite-variables/src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+console.log('// Javascript - Vanilla (Como un juego sin mods)')
+
+console.warn('// ! Formas de mostrar información en la consola')
+
+console.log('Me muestra el texto o variable en color estandar (blanco o azul)') // clg
+console.warn('Me muestra el texto o variable en color amarillo (alerta)') // cwa
+console.error('Me muestra el texto o variable en color rojo (error)') // cer
+
+// Shortcut para duplicar línea (Siempre seleccionado las líneas o línea)
+// Shift + Alt + ⬇️⬆️ || Ctrl + C & Ctrl + V
+
+console.log('// ! Extensiones de Visual Studio Code')
+
+// * JavaScript (ES6) code snippets (xabikos.JavaScriptSnippets)
+// * Template String Converter (meganrogge.template-string-converter)
+
+console.warn('// ! Variable') 
+
+
+
