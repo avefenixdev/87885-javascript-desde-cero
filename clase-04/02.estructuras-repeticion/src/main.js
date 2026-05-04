41891dcb309d9f0fafbd27e4e3241a38beff0c0f
Ave Fenix Dev <avefenixdev@gmail.com>
2026-05-04 11:13:56 -0300

    Terminado el foreach

--- clase-04/02.estructuras-repeticion/src/main.js
+++ clase-04/02.estructuras-repeticion/src/main.js
@@ -1,1 +1,45 @@
 import './style.css'
+
+// ! for
+//                0          1          2         3        4
+let nombres = ['Arnold', 'Gregorio', 'Melina', 'Kevin', 'Kiara']
+console.log(nombres.length) // 5
+// (1) -> Inicializa la variable i (solo una vez)
+// (2) -> Evaluación
+// (3) -> Incremento/Decremento
+// -> (1) -> (2) -> ingreso {} -> (3) -> (2) -> ingreso {} -> (3) -> ... la condición no se cumpla
+//       (1)  ;       (2)       ; (3)
+//debugger
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i])
+}
+
+// Primera iteración
+// i = 0
+// 0 < 5 -> true
+// i < nombres.length 
+// Segunda iteración
+// i = 0
+// 0 < 5 -> true
+// i < nombres.length 
+
+console.warn('// ! foreach (método de los array)')
+// Callback -> una función como argumento de otra
+// function() {} -> función anonima
+// function sumar() {} -> función no anonima
+//            0          1          2         3
+nombres = ['Arnold', 'Gregorio', 'Melina', 'Kevin', 'Kiara']
+//debugger
+nombres.forEach(function(nombre, indice, array) {
+    console.log(nombre) // Arnold -> Gregorio ...
+    console.log(indice) // 0 -> 1
+    console.log(array) // ['Arnold', 'Gregorio', 'Melina', 'Kevin', 'Kiara']
+})
+ 
+// * Arrays Homogeneos // ['Arnold', 'Gregorio', 'Melina', 'Kevin', 'Kiara'] [2, 3, 4, 5]
+// Arrays Heterogeneos  // [2, 'Gregorio', true, [2, 3, 4], {x: 1, y: 2}] 
+
+// ! for in
+// ! for of
+// ! while
+// ! do while
